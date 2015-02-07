f1f3670101caf3c8912209291a61bf5dfb701940
toby <tobytronics@gmail.com>
2015-02-07 18:24:34 +0000

    Every some

---
+++ every-some.js
@@ -0,0 +1,11 @@
+function checkUsersValid(goodUsers) {
+    return function(submittedUsers) {
+        return submittedUsers.every(function(su) {
+            return goodUsers.some(function(gu) {
+               return su.id === gu.id
+            })
+        })
+    }
+}
+
+module.exports = checkUsersValid
