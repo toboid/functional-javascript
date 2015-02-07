184cf014ea7532a078a990b1f974b3778105693e
toby <tobytronics@gmail.com>
2015-02-07 18:56:28 +0000

    Recursion

---
+++ recursion.js
@@ -0,0 +1,8 @@
+function reduce(arr, fn, initial) {
+    return (function reduceOne(index, aggregate) {
+        if(index > arr.length - 1) return aggregate
+        return reduceOne(index+1, fn(initial, arr[index], index, arr))
+    })(0, initial)
+}
+
+module.exports = reduce
