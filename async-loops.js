53f65c83fc944acf6874795c770e6a416fe60429
toby <tobytronics@gmail.com>
2015-02-09 22:20:44 +0000

    Async loop - clean-up use of index

--- async-loops.js
+++ async-loops.js
@@ -1,27 +1,14 @@
 function loadUsers(userIds, load, done) {
     var loaded = 0;
-    var users = userIds;
-    userIds.forEach(function (id) {
+    var users = [];
+    userIds.forEach(function (id, idx) {
         load(id, function (user) {
-            users[users.indexOf(id)] = user;
-            if (++loaded >= userIds.length) {
+            users[idx] = user;
+            if (++loaded === userIds.length) {
                 done(users);
             }
         });
     });
 }
-
-// Model answer
-
-//function loadUsers(userIds, load, done) {
-//    var completed = 0
-//    var users = []
-//    userIds.forEach(function (id, index) {
-//        load(id, function (user) {
-//            users[index] = user
-//            if (++completed === userIds.length) return done(users)
-//        })
-//    })
-//}
 
 module.exports = loadUsers
