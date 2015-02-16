8d7185e6222089c6f04c1ff96fe2767d7edc94e3
toby <tobytronics@gmail.com>
2015-02-16 15:47:10 +0000

    Tweak to currying

--- currying.js
+++ currying.js
@@ -1,10 +1,5 @@
 function curryN(fn, n) {
-    var paramCount;
-    if(n === undefined) {
-        paramCount = fn.length
-    } else {
-        paramCount = n
-    }
+    var paramCount = n || fn.length;
 
     return function(p) {
         if(paramCount === 1) {
@@ -17,31 +12,23 @@ function curryN(fn, n) {
 
 module.exports = curryN
 
-
-// Offical solution
+//// Model answer.
 //function curryN(fn, n) {
-//    // If `n` argument was omitted, use the function .length property.
-//    if (typeof n !== 'number') n = fn.length
+//    if (typeof n !== 'number') n = fn.length;
 //
 //    function getCurriedFn(prev) {
 //        return function(arg) {
-//            // Concat the just-specified argument with the array of
-//            // previously-specified arguments.
-//            var args = prev.concat(arg)
-//            // Not all arguments have been satisfied yet, so return a curried
-//            // version of the original function.
-//            if (args.length < n) return getCurriedFn(args)
-//            // Otherwise, invoke the original function with the arguments and
-//            // return its value.
-//            else return fn.apply(this, args)
+//            var args = prev.concat(arg);
+//
+//            if (args.length < n)
+//                return getCurriedFn(args);
+//            else
+//                return fn.apply(this, args)
 //        };
 //    }
 //
-//    // Return a curried version of the original function.
 //    return getCurriedFn([])
 //}
-//
-//module.exports = curryN
 
 // Solution Source:
 // http://benalman.com/news/2012/09/partial-application-in-javascript/#manually-specifying-function-arity
