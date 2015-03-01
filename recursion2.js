2ad320c14bd9af80adbb570ff60042e98b52223a
toby <tobytronics@gmail.com>
2015-03-01 17:21:28 +0000

    Remove unrequired

--- recursion2.js
+++ recursion2.js
@@ -1,4 +1,4 @@
-// Chained, compact version
+// Uniquely filters the array on each call, but doesn't mutate anything.
 function getDependencies(tree) {
     return Object.keys(tree.dependencies || {})
         .reduce(function (all, name) {
@@ -12,24 +12,6 @@ function getDependencies(tree) {
             return key !== arr[idx - 1];
         });
 }
-
-// Uniquely filters the array on each call, but doesn't mutate anything.
-/*
-function getDependencies(tree) {
-    var dependencyNames = Object.keys(tree.dependencies || {});
-
-    var dependencies = dependencyNames.reduce(function (all, name) {
-        var dependency = tree.dependencies[name];
-        return all
-            .concat(name + "@" + dependency.version)
-            .concat(getDependencies(dependency));
-    }, []);
-
-    return dependencies.sort().filter(function (d, idx, arr) {
-        return d !== arr[idx - 1];
-    });
-}
-*/
 
 // Probably faster as only creates a single array and doesn't have to keep sorting. But mutates the result array.
 /*
